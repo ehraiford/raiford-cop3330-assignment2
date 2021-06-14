34e4f536752ff3a33af8c492019a7b4770f16492
Evan Raiford <84350467+ehraiford@users.noreply.github.com>
2021-06-13 23:52:41 -0400

    Completed exercise 39. Finishing test cases.

--- src/main/java/ex39/solution39.java
+++ src/main/java/ex39/solution39.java
@@ -12,14 +12,57 @@ public class solution39 {
         List<HashMap> employees = new ArrayList<>();
         employees = createArrayList(employees);
 
-      //  Collections.sort(employees, mapComparator);
+        System.out.println("Results:\n\nName                    | Position               |Separation Date");
+        System.out.println("________________________|________________________|________________________");
+        generateOrganized(employees);
 
 
     }
 
+    public static void generateOrganized(List<HashMap> employees) {
+
+        String returnString = "";
+        while(employees.size() > 0){
+            int highest = 0;
+            for(int ticker = 1; ticker < employees.size(); ticker++){
+               highest = findHigher(highest, ticker, employees);
+            }
+            printInfo(employees.get(highest));
+            employees.remove(highest);
+        }
+    }
+
+    public static int findHigher(int highest, int ticker, List<HashMap> employees) {
+        if(employees.get(highest).get("Last").toString().compareTo(employees.get(ticker).get("Last").toString()) > 0)
+            highest = ticker;
+        return highest;
+    }
+
+    //generates string with space characters to format the list
+    //written to handle up to 24 characters, big enough for
+    //all employees from the example but would not work for much larger strings
+    public static String generateSpace(String string) {
+        int numNeeded = 24 - string.length();
+
+        String spaces = "";
+        for(int ticker = 0; ticker < numNeeded; ticker++)
+            spaces = spaces + " ";
+        return spaces;
+    }
+
+    //just prints out information about the employee given to it
+    public static void printInfo(HashMap employee) {
+        System.out.print(employee.get("First") + " " + employee.get("Last"));
+        String nameOutput = employee.get("First").toString()  + " " + employee.get("Last").toString();
+        System.out.print(generateSpace(nameOutput));
+        System.out.print("|" + employee.get("Position").toString());
+        String positionOutput = employee.get("Position").toString();
+        System.out.print(generateSpace(positionOutput));
+        System.out.print("|" + employee.get("Separation") + "\n");
+    }
 
     //creates employee maps and puts it in the ArrayList. Done in separate method to keep main shorter.
-    private static List<HashMap> createArrayList(List<HashMap> employees) {
+    public static List<HashMap> createArrayList(List<HashMap> employees) {
 
         HashMap<String,String> johnson = new HashMap<>();
         johnson.put("First", "John");
@@ -65,10 +108,4 @@ public class solution39 {
 
         return employees;
     }
-
-    private static Comparator<Map<String, String>> mapComparator = new Comparator<Map<String, String>>() {
-        public int compare(Map<String, String> original, Map<String, String> compare) {
-            return original.get("Last").compareTo(compare.get("Last"));
-        }
-    };
 }
--- src/test/java/ex39/solution39Test.java
+++ src/test/java/ex39/solution39Test.java
@@ -1,14 +1,38 @@
+/*
+ *  UCF COP3330 Summer 2021 Assignment 2 Solution
+ *  Copyright 2021 Evan Raiford
+ */
 package ex39;
 
+import ex40.solution40;
 import org.junit.jupiter.api.Test;
 
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class solution39Test {
 
+    @Test
+    void generateSpace_returns_string_with_proper_num_of_spaces(){
+        String space = solution39.generateSpace("Gary Jennings");
+        assertTrue(space.compareTo("           ") == 0);
+    }
 
+    @Test
+    void generateSpace_returns_empty_string_with_too_large_String(){
+        String space = solution39.generateSpace("This exercise was much easier than the last.");
+        assertTrue(space.isEmpty());
+    }
+
+    @Test
+    void findHigher_returns_higher_of_two_options(){
+        List<HashMap> employees = new ArrayList<>();
+        employees = solution39.createArrayList(employees);
+
+       assert
+    }
 }
